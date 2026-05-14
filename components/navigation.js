466662a809a61b2d445567b91e01a5b0a0ac1955
八重樫 海斗 <kaito.yaegashi@gmail.com>
2026-05-14 09:07:10 +0900

    miniture

--- components/navigation.js
+++ components/navigation.js
@@ -2,6 +2,7 @@
     const NAV_LINKS = Object.freeze([
         { label: 'Woviについて', href: '/company.html' },
         { label: 'Kurumi', href: '/index.html#business' },
+        { label: '住宅ミニチュア', href: '/miniature.html' },
         { label: '会社概要', href: '/company.html#company' },
         { label: 'お知らせ', href: '/news.html' },
         { label: 'お問い合わせ', href: '/index.html#contact', isCta: true }
